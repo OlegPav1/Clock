385064f31a208979ea5af451a46cc6402766148d
Oleg <Teacher.med.14@gmail.com>
2021-09-07 11:16:20 +0300

    commit 1

---
+++ Clock.py
@@ -0,0 +1,17 @@
+from time import sleep
+import datetime
+from datetime import timedelta
+date = datetime.datetime.now()
+for h in range(25):
+    if h<25:
+        for m in range(61):
+                for my_time in range(61):
+                        date = date + timedelta(seconds=1)
+                        print(date.strftime('%H:%M:%S'), end='')
+                        sleep(1)
+                        my_time +=1
+                        print('\r', end='')
+        m +=1
+    else:
+        h=-1
+h +=1
